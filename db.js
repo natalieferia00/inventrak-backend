725237910d58fa1a9a03d3841c78c967d8942caa
natalieferia00 <natalieferia00@gmail.com>
2026-04-02 18:50:05 -0500

    commit

--- db.js
+++ db.js
@@ -1,7 +1,15 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Creamos el pool usando la URL completa de la variable de entorno
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 module.exports = pool.promise();
